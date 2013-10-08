577c95970cae036c544c04d65e7438b2e4debfaa
rubychilds <ruby.childs1@gmail.com>
2013-10-08 15:00:46 -0400

    started adding bits to fast - main needs work as does drawing the output

--- algAss3/Fast.java
+++ algAss3/Fast.java
@@ -1,4 +1,28 @@
 public class Fast {
- public static void main(String[] args){}
+ public static void main(String[] args){
+     Point[] points = readPoints(args[0]);
+
+     for(int p = 0; p < points.length; p++){
+         points[p].draw();
+         for(int otherP = 0; otherP < points.length; p++ ){
+             if(p != otherP)
+                 points[p].slopeTo(points[otherP]);
+         }
+             
+     
+     }
+         
+         
+ }
+ 
+ 
+  private static Point[] readPoints(String filename) {
+      In filein = new In(filename);
+      int n = filein.readInt();
+      Point[] points = new Point[n];
+      for (int i = 0; i < n; i++)
+         points[i] = new Point(filein.readInt(), filein.readInt());
+      return points;
+ }
 
 }
